d83ae4b918c5b9aee58375cd2534f03b5b1f2d85
Ulugbek <ulugbekmuslitdinov@gmail.com>
2023-03-28 00:59:52 -0700

    hashTable created

---
+++ HashMap/src/MyHashMap.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author User
+ *
+ */
+public class MyHashMap {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
