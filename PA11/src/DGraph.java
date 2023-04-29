4779be44dd92a2f3ccf8486ab1b67fe37b5fda3a
Ulugbek <ulugbekmuslitdinov@gmail.com>
2023-04-28 22:39:12 -0700

    own function and timing

--- PA11/src/DGraph.java
+++ PA11/src/DGraph.java
@@ -1,6 +1,17 @@
 import java.util.*;
 
+
+/**
+ * This class represents a directed graph
+ */
 public class DGraph {
+
+    /**
+     * This class represents a node in the graph
+     * Each node has a key and a value
+     * Each node also has a list of edges that are adjacent to it
+     * Each edge has a sink node and a weight
+     */
     private class Node {
         int key;
         int value;
@@ -14,6 +25,10 @@ public class DGraph {
 
     Map<Integer, Node> nodes = new HashMap<>();
 
+    /**
+     * This class represents an edge in the graph
+     * Each edge has a sink node and a weight
+     */
     private class Edge {
         Node sink;
         double weight;
@@ -24,6 +39,12 @@ public class DGraph {
         }
     }
 
+    /**
+     * This class represents a pair of a cost and a node
+     * This is used in the heuristic method
+     * The heuristic method returns a pair of the cost and the last node visited
+     * This class is a helper class for the heuristic method to allow the function to return two values
+     */
     private class Pair {
         double cost;
         Node lastNode;
@@ -34,14 +55,25 @@ public class DGraph {
         }
     }
 
+    /**
+     * This method adds a node to the graph
+     *
+     * @param key   Key of the node
+     * @param value Value of the node
+     */
     public void addNode(int key, int value) {
         nodes.put(key, new Node(key, value));
     }
 
+    /**
+     * This method adds an edge to the graph
+     * It also adds the nodes if they are not already in the graph
+     *
+     * @param k1 Key of the first node
+     * @param k2 Key of the second node
+     * @param w  Weight of the edge
+     */
     public void addEdge(int k1, int k2, double w) {
-//        Node u = nodes.get(k1);
-//        Node v = nodes.get(k2);
-//        u.adjList.add(new Edge(v, w));
         if (!nodes.containsKey(k1)) {
             addNode(k1, k1);
         }
@@ -51,6 +83,11 @@ public class DGraph {
         nodes.get(k1).adjList.add(new Edge(nodes.get(k2), w));
     }
 
+    /**
+     * This method returns string representation of the graph
+     *
+     * @return string representation of the graph
+     */
     public String toString() {
         String str = "";
         for (int key : nodes.keySet()) {
@@ -65,6 +102,12 @@ public class DGraph {
         return str;
     }
 
+    /**
+     * This method implements the heuristic method
+     *
+     * @param startKey Key of the starting node
+     * @return cost of the path
+     */
     public double heuristic(int startKey) {
         LinkedList<Integer> path = new LinkedList<>();
         path.add(startKey);
@@ -94,7 +137,7 @@ public class DGraph {
                 lastNode = minEdgeNode;
             }
         }
-        for (Edge e: lastNode.adjList) {
+        for (Edge e : lastNode.adjList) {
             if (e.sink.key == startKey) {
                 cost += e.weight;
                 break;
@@ -106,6 +149,12 @@ public class DGraph {
         return cost;
     }
 
+    /**
+     * This method implements the backtracking method
+     *
+     * @param startKey Key of the starting node
+     * @return cost of the path
+     */
     public double backtracking(int startKey) {
         LinkedList<Integer> path = new LinkedList<>();
         path.add(startKey);
@@ -117,7 +166,7 @@ public class DGraph {
         Pair result = backtrackingHelper(queue, path, cost, lastNode);
         cost = result.cost;
         lastNode = result.lastNode;
-        for (Edge e: lastNode.adjList) {
+        for (Edge e : lastNode.adjList) {
             if (e.sink.key == startKey) {
                 cost += e.weight;
                 break;
@@ -128,6 +177,15 @@ public class DGraph {
         return cost;
     }
 
+    /**
+     * This is a helper method for the backtracking method
+     *
+     * @param queue    Queue of nodes
+     * @param path     Path of nodes
+     * @param cost     Cost of the path
+     * @param lastNode Last node visited
+     * @return Pair of cost and last node visited
+     */
     private Pair backtrackingHelper(LinkedList<Node> queue, LinkedList<Integer> path, double cost, Node lastNode) {
         Node current = queue.pollFirst();
         double minEdgeWeight = Double.MAX_VALUE;
@@ -150,5 +208,71 @@ public class DGraph {
             lastNode = result.lastNode;
         }
         return new Pair(cost, lastNode);
+    }
+
+    /**
+     * This method is my own method
+     * It loops through all the edges and finds the minimum edge
+     * It then adds the node of the minimum edge to the path
+     * It repeats this process until all nodes are visited
+     *
+     * @param startKey Key of the starting node
+     * @return cost of the path
+     */
+    public double own_method(int startKey) {
+        LinkedList<Integer> path = new LinkedList<>();
+        Set<Integer> toVisit = nodes.keySet();
+        path.add(startKey);
+        Node startNode = nodes.get(startKey);
+        double count = 0;
+
+        Node minEdgeNode = null;
+        double minEdgeWeight = Double.MAX_VALUE;
+        for (int i = 0; i < toVisit.size(); i++) {
+            for (Edge e : startNode.adjList) {
+                if (!path.contains(e.sink.key)) {
+                    if (e.weight < minEdgeWeight) {
+                        minEdgeWeight = e.weight;
+                        minEdgeNode = e.sink;
+                    }
+                }
+            }
+            if (minEdgeNode != null) {
+                path.add(minEdgeNode.key);
+                count += minEdgeWeight;
+                startNode = minEdgeNode;
+                minEdgeNode = null;
+                minEdgeWeight = Double.MAX_VALUE;
+            }
+        }
+        for (Edge e : startNode.adjList) {
+            if (e.sink.key == startKey) {
+                count += e.weight;
+                break;
+            }
+        }
+        count = Math.round(count * 10) / 10.0;
+        System.out.println("cost = " + count + ", visitOrder = " + path);
+        return count;
+    }
+
+    public void timing(int startKey) {
+        long startTime = System.nanoTime();
+        double cost = heuristic(startKey);
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1000000;
+        System.out.println("heuristic: cost=" + cost + ", " + duration + " milliseconds");
+
+        startTime = System.nanoTime();
+        cost = backtracking(startKey);
+        endTime = System.nanoTime();
+        duration = (endTime - startTime) / 1000000;
+        System.out.println("backtracking: cost=" + cost + ", " + duration + " milliseconds");
+
+        startTime = System.nanoTime();
+        double count = own_method(startKey);
+        endTime = System.nanoTime();
+        duration = (endTime - startTime) / 1000000;
+        System.out.println("own_method: cost=" + count + ", " + duration + " milliseconds");
     }
 }
--- PA11/src/PA11Main.java
+++ PA11/src/PA11Main.java
@@ -1,9 +1,21 @@
+/**
+ * PA11Main.java
+ * @author: Ulugbek Muslitdinov
+ * @class: CSC210 Spring 2023
+ */
+
 import java.util.List;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
+/**
+ * This class is the main class that reads the file and calls the heuristic and
+ * backtracking methods from DGraph.java
+ * It accepts two command line arguments: the name of the file and the command
+ * (HEURISTIC or BACKTRACK)
+ */
 public class PA11Main {
 
     public static void main(String[] args) {
@@ -11,8 +23,9 @@ public class PA11Main {
         List<Integer> path = new ArrayList<>();
         String fileName = args[0];
         String command = args[1];
-        DGraph dg = new DGraph();
+        DGraph dg = new DGraph(); // create a new DGraph object
 
+        // read the file
         try {
             File file = new File(fileName);
             Scanner fileReader = new Scanner(file);
@@ -20,7 +33,7 @@ public class PA11Main {
             while (fileReader.hasNextLine()) {
                 String line = fileReader.nextLine();
                 if (line.charAt(0) != '%') {
-                    if (firstLine == false) {
+                    if (firstLine == false) { // read the first line
                         String[] items = line.split("\\s+");
                         if (items.length == 3) {
                             nodes = Integer.parseInt(items[1]);
@@ -41,13 +54,22 @@ public class PA11Main {
                 }
             }
             fileReader.close();
+
+            // call the heuristic or backtracking method
             if (command.equals("HEURISTIC")) {
                 dg.heuristic(1);
             }
             if (command.equals("BACKTRACK")) {
                 dg.backtracking(1);
             }
+            if (command.equals("OWN")) {
+                dg.own_method(1);
+            }
+            if (command.equals("TIME")) {
+                dg.timing(1);
+            }
 
+            // if file not found, print "File not found"
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
